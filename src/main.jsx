c4af10ec44aa1fece73418283d666cf89f941f81
Jesus <jesusg.1301.jg@gmail.com>
2023-04-11 12:46:45 -0400

    running changes to implement pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { register } from '../service-worker';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+register();
