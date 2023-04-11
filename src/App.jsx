144233c4d61a6db4fc8b6d9553c1534b77331758
Jesus <jesusg.1301.jg@gmail.com>
2023-04-11 12:26:23 -0400

    running changes to implement pwa

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testi
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('../service-worker.js')
       .then(function(registration) {
         console.log('Service worker registered successfully:', registration.scope);
       })
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hoobank-modern-ui-landing-page.vercel.app/',
   plugins: [
     react(),
     VitePWA({
